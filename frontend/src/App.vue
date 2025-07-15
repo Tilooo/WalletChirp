e4de882970957819d429feed662a101a726813c4
Tilooo <moonaviad@gmail.com>
2025-07-15 15:54:09 +0300

    2. Successfully set up Vue.js frontend with Tailwind CSS.

---
+++ frontend/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-slate-900 text-slate-200 min-h-screen flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-cyan-400">WalletChirp is Ready!</h1>
+  </div>
+</template>
